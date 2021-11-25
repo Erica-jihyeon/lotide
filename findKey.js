3dc7304290703fd0bfe4953dfc41cbef4a724f54
Erica-jihyeon <jihyeon20@gmail.com>
2021-11-25 22:54:43 +0000

    test added

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`💛💛💛Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (objs, callback) => {
+  for(const key in objs) {
+    if(callback(objs[key])) {
+      return key;
+    }
+  }
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
