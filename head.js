1cbc66e38915a016403ac8e6b5f660f506ffeb1c
Erica-jihyeon <jihyeon20@gmail.com>
2021-11-29 16:47:57 +0000

    assertEqual and head function seperate with test codes(W02D1)

--- head.js
+++ head.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    //return console.log(`Assertion Passed: ${actual} === ${expected}`);
-    return console.log(`💛💛💛Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = (arr) => {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), 5);
-assertEqual(head([6,7,9]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hi", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), 5);
+assertEqual(head([6,7,9]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hi", "Lighthouse", "Labs"]), "Hello");
