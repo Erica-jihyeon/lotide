e4a2701171d22792f6312aa8afb01b45a9f6253c
Erica-jihyeon <jihyeon20@gmail.com>
2021-11-29 22:08:56 +0000

    add one more test func

--- test/headTest.js
+++ test/headTest.js
@@ -11,6 +11,10 @@ describe("#head", () => {
     assert.strictEqual(head(['5']), '5');
   });
 
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
