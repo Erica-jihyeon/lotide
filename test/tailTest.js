f8580f3f20f25f9d25c226d8f1555ccc1a01fbe9
Erica-jihyeon <jihyeon20@gmail.com>
2021-11-29 22:11:20 +0000

    coverted tailTest.js with M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,25 @@
-const assertEqual = require('../assertEqual');
+/*const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, 3); // original array should still have 3 elements!*/
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  })
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  })
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  })
+
+})
