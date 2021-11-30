0b40c38504b792ae9e37eb6b057a2375ce01045d
Erica-jihyeon <jihyeon20@gmail.com>
2021-11-30 16:35:59 +0000

    converted to M&C for flatten test

--- flatten.js
+++ flatten.js
@@ -1,28 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  let isSame = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      isSame = false;
-      return false;
-    }
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  if (isSame) {
-    return true;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    //return console.log(`Assertion Passed: ${actual} === ${expected}`);
-    return console.log(`💛💛💛Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const flatten = (arr) => {
   let flattenedArr = [];
   for(const elem of arr) {
@@ -36,6 +11,4 @@ const flatten = (arr) => {
   }
   return flattenedArr;
 }
-
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten([1, 2, [3, 4, 5], [6, 7], [8, 9]]));
+module.exports = flatten;
---
+++ test/flattenTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const flatten = require('../flatten');
+
+describe("flatten", () => {
+
+  it("return [1, 2, 3, 4, 5, 6] for [1, 2, [3, 4], 5, [6]]", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  });
+
+  it("return [1, 2, 3, 4, 5, 6, 7, 8, 9] for [1, 2, [3, 4, 5], [6, 7], [8, 9]]", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4, 5], [6, 7], [8, 9]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+})
